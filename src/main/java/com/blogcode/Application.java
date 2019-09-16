b7eec5edce9a0f5e5bdf8b1a9558e920a2cb4951
yunheur <yunheur@gmail.com>
2019-09-16 23:06:22 +0900

    change source code

--- src/main/java/com/blogcode/Application.java
+++ src/main/java/com/blogcode/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(Application.class, args);
 	}
 }
